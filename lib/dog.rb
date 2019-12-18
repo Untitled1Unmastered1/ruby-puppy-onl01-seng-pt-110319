69b08100e53c2cd1fc937bb77effe3723cac456c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-18 00:10:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,18 @@
-Class Dog 
+class Dog
+
+  attr_accessor :name
 
   @@all = []
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-    @@all << self
-  end 
-  
-  def self.all
-    @@all 
-  end 
 
-  def self.print_all
-    @@all.each do |d|
-      puts d.name 
-  end 
-end 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
     
     
